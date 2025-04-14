df999366c164abb64f455a89b15d6b8785b08ad6
RubenCorchete <rubencorchete01@gmail.com>
2025-04-14 12:43:54 +0200

    add: comentarios spwan_jugador.gd

--- scripts/spawn_jugador.gd
+++ scripts/spawn_jugador.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+# Variable booleana que indica si el área está vacía (sin cuerpos ni otras áreas superpuestas)
 var estaVacio: bool:
 	get:
+		# Devuelve true si NO hay áreas ni cuerpos superpuestos, es decir, si está completamente libre
 		return (!has_overlapping_areas() && !has_overlapping_bodies())
