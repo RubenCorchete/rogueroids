def653ffd8658143cce75686fa70dae38ac19166
RubenCorchete <rubencorchete01@gmail.com>
2025-04-14 11:48:13 +0200

    add: hud.gd comentado

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,15 +1,21 @@
+# Script del HUD principal. Controla la visualización de las vidas y la puntuación.
 extends Control
 
+# Contenedor de vidas
 @onready var vidas = $Vidas
+
+# Etiqueta que muestra la puntuación del jugador.
 @onready var puntuacion = $Puntuacion:
 	set(value):
 		puntuacion.text = "SCORE: " + str(value)
 
 var uiEscena = preload("res://scennes/ui_vidas.tscn")
 
+# Esta función cambia la puntuación mostrada
 func cambiarScore(cantidad):
 	puntuacion.text = "SCORE: " + str(cantidad)
 
+# Actualiza las vidas mostradas
 func iniciarVidas(cantidad):
 	for ul in vidas.get_children():
 		ul.queue_free()
