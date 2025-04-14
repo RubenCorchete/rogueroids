47818c4893543e785a15150722b09fd31b3c15f5
RubenCorchete <rubencorchete01@gmail.com>
2025-04-14 12:00:20 +0200

    add: comentarios a laser.gd

--- scripts/laser.gd
+++ scripts/laser.gd
@@ -1,17 +1,21 @@
+# Script del laser disparado por las naves
 extends Area2D
 
+# Vector que indica la dirección a la que va a salir el laser.
 var vectorDeMovimiento := Vector2(0, -1)
+
+# Velocidad del laser
 @export var velocidad := 500.0
 
 func _physics_process(delta):
+	# Mueve el láser en la dirección de su rotación, multiplicado por su velocidad y el tiempo entre frames.
 	global_position += vectorDeMovimiento.rotated(rotation) * velocidad * delta
-	
 
-
+# Cuando el laser sale de la pantalla se elimina
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
 
-
+# Si el laser toca un asteroide lo hace explotar
 func _on_area_entered(area: Area2D) -> void:
 	if area is asteroide:
 		var asteroid = area
